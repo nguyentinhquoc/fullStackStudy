38a1dd2caa2007fda2fd51b19aebbeb0deea2893
Nguyễn Quốc Tình <125818904+nguyentinhquoc@users.noreply.github.com>
2025-07-25 09:19:59 +0700

    update day 22 vs 23

---
+++ Javascript/BTVN/Day22vs23/main.js
@@ -0,0 +1,250 @@
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const addBtn = $(".add-btn");
+const modalClose = $(".modal-close");
+const cancelBtn = $("#addTaskModal .btn-secondary");
+const addTaskModal = $("#addTaskModal");
+const inputsAddTaskModal = $$(".todo-app-form input");
+const formAddTask = $(".todo-app-form");
+const btnAddTask = $("#addTaskModal .btn-primary");
+const inputSearch = $('input[class="search-input"]')
+inputSearch.addEventListener('input', (e) => {
+  const inputValue = e.target.value.toUpperCase()
+  $$('.task-card').forEach(element => {
+    const textElement = element.querySelector('.task-title').innerText.toUpperCase()
+    if (!textElement.includes(inputValue)) element.hidden = true
+    else element.hidden = false
+  });
+})
+let todoTasks = localStorage.getItem("listTasks")
+  ? JSON.parse(localStorage.getItem("listTasks"))
+  : [];
+// click thêm task
+addBtn.addEventListener("click", () => {
+  //  1: Xử lý mở/đóng modal
+  inputsAddTaskModal.forEach((element) => {
+    element.value = null;
+  });
+  $("#taskDescription").value = null;
+  delete addTaskModal.dataset.index;
+  toggleModal();
+});
+//click ẩn modal
+cancelBtn.onclick = () => {
+  toggleModal();
+};
+modalClose.onclick = () => {
+  toggleModal();
+};
+// hàm ẩn hiện
+function toggleModal() {
+  addTaskModal.classList.toggle("show");
+  if (addTaskModal.classList.contains("show")) {
+    // 2: Tự động focus input
+    setTimeout(() => {
+      inputsAddTaskModal[0].focus();
+    }, 50);
+    inputsAddTaskModal.forEach((element) => {
+      element.classList.remove("input-error");
+      element.style.borderColor = "#d1d5db";
+    });
+  }
+}
+// 3: Xử lý form submission
+formAddTask.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
+// 4:
+function addTask() {
+  if (!validateData()) return;
+  const newTask = {
+    title: $("#taskTitle").value,
+    description: $("#taskDescription").value,
+    category: $("#taskCategory").value,
+    priority: $("#taskPriority").value,
+    startTime: $("#startTime").value,
+    endTime: $("#endTime").value,
+    DueDate: $("#taskDate").value,
+    cardColor: $("#taskColor").value,
+    completed: false,
+  };
+  todoTasks.unshift(newTask);
+  localStorage.setItem("listTasks", JSON.stringify(todoTasks));
+  renderData();
+  toggleModal();
+}
+function editTask() {
+  if (!validateData()) return;
+  const newTask = {
+    title: $("#taskTitle").value,
+    description: $("#taskDescription").value,
+    category: $("#taskCategory").value,
+    priority: $("#taskPriority").value,
+    startTime: $("#startTime").value,
+    endTime: $("#endTime").value,
+    DueDate: $("#taskDate").value,
+    cardColor: $("#taskColor").value,
+    completed: false,
+  };
+  const index = addTaskModal.dataset.index;
+  todoTasks[index] = { ...todoTasks[index], ...newTask };
+  localStorage.setItem("listTasks", JSON.stringify(todoTasks));
+  renderData();
+  toggleModal();
+}
+function deleteTask(index) {
+  todoTasks.splice(index, 1);
+  localStorage.setItem("listTasks", JSON.stringify(todoTasks));
+  renderData();
+}
+renderData();
+// Trống XXS
+function blockXss(stringHtml) {
+  let element = document.createElement("div");
+  element.innerText = stringHtml;
+  return element.innerHTML;
+}
+//update Data
+function updateData(index, obj) {
+  todoTasks[index] = { ...todoTasks[index], ...obj };
+  localStorage.setItem("listTasks", JSON.stringify(todoTasks));
+  renderData();
+}
+
+// render dữ liệu ra màn hì
+// nh
+const btnTad = $(".tab-list").querySelectorAll(".tab-button");
+btnTad.forEach((element) => {
+  element.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    $(".tab-button.active").classList.remove("active");
+    e.target.classList.add("active");
+    renderData()
+    // if (e.target.classList.contains("action")) {
+    //   $$('.task-card').forEach((element) => {
+    //     if (element.classList.contains('completed')) {
+    //       element.hidden = true
+    //     }
+    //     if (!element.classList.contains('completed')) {
+    //       element.hidden = false
+    //     }
+    //   })
+    // } else {
+    //   $$('.task-card').forEach((element) => {
+    //     if (element.classList.contains('completed')) {
+    //       element.hidden = false
+    //     }
+    //     if (!element.classList.contains('completed')) {
+    //       element.hidden = true
+    //     }
+    //   })
+    // }
+  });
+});
+function renderData() {
+  let stringHtmlRender = "";
+  todoTasks.forEach((element, index) => {
+    let isCompleted = false
+    if ($('.tab-button.action').classList.contains('active')) {
+      isCompleted = false
+    }
+    if ($('.tab-button.completed').classList.contains('active')) {
+      isCompleted = true
+    }
+    if (isCompleted === element.completed) {
+      stringHtmlRender += `
+    <div class="task-card ${blockXss(element.cardColor)} ${element.completed ? "completed" : ""
+        }"  data-index="${index}" >
+        <div class="task-header">
+          <h3 class="task-title">${blockXss(element.title)}</h3>
+          <button class="task-menu">
+            <i class="fa-solid fa-ellipsis fa-icon"></i>
+            <div class="dropdown-menu">
+              <div class="dropdown-item edit">
+                <i class="fa-solid fa-pen-to-square fa-icon"></i>
+                Edit
+              </div>
+              <div class="dropdown-item complete">
+                <i class="fa-solid fa-check fa-icon"></i>
+                Mark as Active
+              </div>
+              <div class="dropdown-item delete">
+                <i class="fa-solid fa-trash fa-icon"></i>
+                Delete
+              </div>
+            </div>
+          </button>
+        </div>
+        <p class="task-description">${blockXss(element.description)}</p>
+        <div class="task-time">${blockXss(element.startTime)} - ${blockXss(
+          element.endTime
+        )}</div>
+      </div>
+    `;
+    }
+  });
+  $(".task-grid").innerHTML = stringHtmlRender;
+}
+// click nút complete
+$(".task-grid").addEventListener("click", (e) => {
+  const index = e.target.closest(".task-card");
+  if (e.target.closest(".complete")) {
+    e.stopPropagation();
+    updateData(index, { completed: true });
+  } else if (e.target.closest(".edit.dropdown-item")) {
+    // Mở model setData
+    toggleModal();
+    addTaskModal.querySelector(".modal-title").innerText = "Edit Task";
+    btnAddTask.innerText = "Edit Task";
+    addTaskModal.dataset.index = index;
+    $("#taskTitle").value = todoTasks[index].title;
+    $("#taskDescription").value = todoTasks[index].description;
+    $("#taskCategory").value = todoTasks[index].category;
+    $("#taskPriority").value = todoTasks[index].priority;
+    $("#startTime").value = todoTasks[index].startTime;
+    $("#endTime").value = todoTasks[index].endTime;
+    $("#taskDate").value = todoTasks[index].DueDate;
+    $("#taskColor").value = todoTasks[index].cardColor;
+  } else if (e.target.closest(".delete.dropdown-item")) {
+    if (confirm("Bạn có chắc muốn xóa task này ?")) deleteTask(index);
+  }
+});
+// Kiểm tra dữ liệu
+function validateData() {
+  let isCheckValidate = true;
+  inputsAddTaskModal.forEach((element) => {
+    if (!element.value) {
+      element.style.borderColor = "red";
+      element.classList.add("input-error");
+      isCheckValidate = false;
+    } else {
+      isCheckValidate = true;
+    }
+    element.addEventListener("blur", function () {
+      if (!this.value) {
+        this.style.borderColor = "red";
+        this.classList.add("input-error");
+        isCheckValidate = false;
+      }
+    });
+    element.addEventListener("input", function () {
+      if (this.value) {
+        this.style.borderColor = "#d1d5db";
+        this.classList.remove("input-error");
+      } else {
+        this.style.borderColor = "red";
+        this.classList.add("input-error");
+        isCheckValidate = false;
+      }
+    });
+  });
+  return isCheckValidate;
+}
+// Cập nhập danh sách
+btnAddTask.addEventListener("click", (e) => {
+  if (!addTaskModal.dataset.index) {
+    addTask();
+  } else {
+    editTask();
+  }
+});
